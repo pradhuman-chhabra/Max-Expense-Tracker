1c405c11be8e9994d115dd219286c5d2a04622b2
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2023-08-15 23:14:24 +0530

    made the hardcoded data into dynamic using curly braces

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,6 +4,7 @@ function ExpenseItem() {
   const expenseDate = new Date(2021, 2, 28);
   const expenseTitle = "Car Insurance";
   const expenseAmount = 294.67;
+  // here ahat we have done with the variables is that we have removed hard coded text and added them in order to make them dynamic , this is to understand better that we can now use props and insted of storing data in these variables we can just recieve them via props
 
   return (
     // the things about jsx is that we can replace hard coded text with curly braces , we can even put javascript logic inside them, eg anything in js can be put inside these braces
@@ -17,6 +18,7 @@ function ExpenseItem() {
     <>
       <div className="expense-item">
         <div>{expenseDate.toISOString()}</div>
+        {/* Since date is a date object we can't just output the date object as such as a string , we can use toISOString to output date object as a string */}
         <div className="expense-item__description">
           <h2>{expenseTitle}</h2>
           <div className="expense-item__price">${expenseAmount}</div>
