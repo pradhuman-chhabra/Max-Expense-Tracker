22343f446160c5300bd5391164d5ec675e646edb
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2023-08-16 23:23:51 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // here we will store the expenses into an expense array in which every object is a js object
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      {/* Here we can add attributes called props , here we will be able to access the title toilet paper from the above expenses array
+      ,Now these expense componenets have access to the data as its been passed as props*/}
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-  // here ahat we have done with the variables is that we have removed hard coded text and added them in order to make them dynamic , this is to understand better that we can now use props and insted of storing data in these variables we can just recieve them via props
+function ExpenseItem(props) {
+  // in regular js we use paramenters for passing data into functions
+
+  // since we will be using props we can easily comment out these
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+
+  // here what we have done with the variables is that we have removed hard coded text and added them in order to make them dynamic , this is to understand better that we can now use props and insted of storing data in these variables we can just recieve them via props
 
   return (
     // the things about jsx is that we can replace hard coded text with curly braces , we can even put javascript logic inside them, eg anything in js can be put inside these braces
@@ -15,13 +19,23 @@ function ExpenseItem() {
     // props are one of the most important things they make our components reusable , we just used these culry braces to add dynamic values inside jsx, so we will add props.vales inside these curly braces , that's how we will give different values to the same componenet and make it reusable
     // Props are the attributes of your custom HTML elements , i.e Componenets
     // componenets should recive the values from outside through props or exports , but mainly props so the variables will be stored inside the app.js file
+
+    // We can pass data to the custom component by adding a attribute. And inside of that component, we can then get access to all these attributes which might have been set on our custom component. which might have been set on our custom component. Again, we're basically building our own custom HTML elements. our own custom HTML elements. And just as HTML elements can have attributes, it turns out that with React, our own custom components can also have attributes. our own custom components can also have attributes. There, this concept is just called props instead of attributes. instead of attributes. And props simply stands for properties. We can set properties of our own custom components. Now, how does this props concept work though?
+
     <>
       <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         {/* Since date is a date object we can't just output the date object as such as a string , we can use toISOString to output date object as a string */}
         <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-          <div className="expense-item__price">${expenseAmount}</div>
+          <h2>{props.title}</h2>
+          {/* React will ensure that we get one parameter in every component , like we're doing it here for ExpenseItem. And that one parameter will be an object which holds all the received attributes as properties, hence the name props for the overall concept. Therefore, we get one parameter, and you can name this parameter whatever you want. eg. You could name it data. But, typically, it's named props to make it clear that this is this is an object which holds all the values we get for the attributes on our custom element i.e components.
+          
+          And to be precise, we get key-value pairs in this props object here which is passed in by React automatically. The keys will be the attribute names defined here, so title, amount, and date in my case {Key is the name you picked for your attribute} And the values, of course, will be the values set here.
+          
+          title={expenses[0].title}
+          Key  : Value  pair
+          So if I now wanna output the title received here in this h2 tag, I can now access props.title like this */}
+          <div className="expense-item__price">${props.amount}</div>
         </div>
       </div>
     </>
